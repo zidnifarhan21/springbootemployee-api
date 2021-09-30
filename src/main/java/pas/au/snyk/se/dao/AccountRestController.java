df2a79b3de0816a2c6711bcacd9699682cd11916
Pas Apicella <pas.apicella@gmail.com>
2021-09-30 10:20:08 +1000

    Add code test for SQL Injection

---
+++ src/main/java/pas/au/snyk/se/dao/AccountRestController.java
@@ -0,0 +1,26 @@
+package pas.au.snyk.se.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/accounts")
+public class AccountRestController {
+
+    private AccountDAO accountDAO;
+
+    @Autowired
+    public AccountRestController(AccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
+    @GetMapping (value = "/{customerId}")
+    public List<AccountDTO> findAccountsByCustomerId (@PathVariable String customerId) {
+        return accountDAO.unsafeJpaFindAccountsByCustomerId(customerId);
+    }
+}
