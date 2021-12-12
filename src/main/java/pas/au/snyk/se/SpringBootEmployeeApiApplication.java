c659d1f01eb4ef61688fbb197c086a919ac4c1db
Pas Apicella <pas.apicella@gmail.com>
2021-12-13 10:48:39 +1100

    Test commit

--- src/main/java/pas/au/snyk/se/SpringBootEmployeeApiApplication.java
+++ src/main/java/pas/au/snyk/se/SpringBootEmployeeApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootEmployeeApiApplication {
 
+    /* fake comment for now **/
     public static void main(String[] args) {
         SpringApplication.run(SpringBootEmployeeApiApplication.class, args);
     }
